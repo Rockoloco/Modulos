1ef1b74d46e29531a88dccb06ed7da113946dcc1
Rodrigo Yañez <rodrigo_yrodriguez@hotmail.com>
2020-09-08 22:41:44 -0500

    Fin sección 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+//Exportación de las graficas
+import {ChartsModule} from 'ng2-charts';
+
+//Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -25,7 +36,11 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ChartsModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+   
+   //Estas son salidas hacia el componente hijo
+   progreso1: number = 25;
+   progreso2: number = 35;
 
+   
+   
+   //Aqui ya trae los valores del padre hacia el hijo
+   get getProgreso1() {
+     return `${this.progreso1}%`;
+   }
+
+   get getProgreso2() {
+    return `${this.progreso2}%`;
+  }
+  
+  
+  
+  
+  
+  //Estas son entradas
+  //Cuando recibes un evento
+  cambioValorHijo(valor:number) {
+    console.log('Hey!!!', valor);
+  }
 }
